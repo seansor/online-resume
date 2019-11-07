26e68585f04149fb6de825d2634e6de2b4ff466a
seansor <soriordan890@gmail.com>
2019-11-07 11:53:03 +0000

    "Updates personal information and adds github api"

--- static/js/maps.js
+++ static/js/maps.js
@@ -9,20 +9,10 @@ function initMap() {
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    var locations = [{
-            lat: 40.785091,
-            lng: -73.968285
-        },
-
-        {
-            lat: 41.084045,
-            lng: -73.874256
-        },
-
-        {
-            lat: 40.754932,
-            lng: -73.984016
-        }
+    var locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874245 },
+        { lat: 40.754932, lng: -73.984016 }
     ];
 
     var markers = locations.map(function(location, i) {
